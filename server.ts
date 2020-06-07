89fb1fbc3d5f13aa9dd7a1469b6e408d635369c1
Robin Wieruch <wrobin@gmx.net>
2020-06-07 12:23:49 +0200

    http-methods

--- server.ts
+++ server.ts
@@ -1,7 +1,28 @@
-import { Application } from 'https://deno.land/x/oak/mod.ts';
+import { Application, Router } from 'https://deno.land/x/oak/mod.ts';
 
 const port = 8000;
 const app = new Application();
+
+const router = new Router();
+
+router.get('/', (ctx) => {
+  ctx.response.body = 'Received a GET HTTP method';
+});
+
+router.post('/', (ctx) => {
+  ctx.response.body = 'Received a POST HTTP method';
+});
+
+router.put('/', (ctx) => {
+  ctx.response.body = 'Received a PUT HTTP method';
+});
+
+router.delete('/', (ctx) => {
+  ctx.response.body = 'Received a DELETE HTTP method';
+});
+
+app.use(router.allowedMethods());
+app.use(router.routes());
 
 app.addEventListener('listen', () => {
   console.log(`Listening on: localhost:${port}`);
