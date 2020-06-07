72a5ff72bf0583341035218b5d0170f82922adfa
Robin Wieruch <wrobin@gmx.net>
2020-06-07 12:23:07 +0200

    init

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
-# deno-oak-example
+# deno-oak-rest-api-example
 
-[Read more about it.](https://www.robinwieruch.de/deno-oak)
+[Read more about it.](https://www.robinwieruch.de/deno-oak-rest-api)
 
 ## Installation
 
-- `git clone git@github.com:the-road-to-deno/deno-oak.git`
-- `cd deno-oak`
+- `git clone git@github.com:the-road-to-deno/deno-oak-rest-api.git`
+- `cd deno-oak-rest-api`
 - `deno run --allow-net index.ts`
 - visit `http://localhost:8000`
--- server.ts
+++ server.ts
@@ -1,41 +1,7 @@
-import {
-  Application,
-  Router,
-  Context,
-} from 'https://deno.land/x/oak/mod.ts';
+import { Application } from 'https://deno.land/x/oak/mod.ts';
 
 const port = 8000;
 const app = new Application();
-
-const routerOne = new Router();
-
-routerOne.get('/1', (ctx) => {
-  ctx.response.body = 'Hello Deno 1';
-});
-
-const routerTwo = new Router();
-
-routerTwo.get('/2', (ctx) => {
-  ctx.response.body = 'Hello Deno 2';
-});
-
-app.use(routerOne.routes());
-app.use(routerOne.allowedMethods());
-
-app.use(routerTwo.routes());
-app.use(routerTwo.allowedMethods());
-
-const logging = async (ctx: Context, next: Function) => {
-  console.log(`HTTP ${ctx.request.method} on ${ctx.request.url}`);
-  await next();
-};
-
-app.use(logging);
-
-app.use((ctx) => {
-  console.log('returning a response ...');
-  ctx.response.body = 'Hello Deno';
-});
 
 app.addEventListener('listen', () => {
   console.log(`Listening on: localhost:${port}`);
